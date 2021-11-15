918cadb80d00bb8dd792e5153e10ab89f07839f8
Carl Marino <carlcmarino@protonmail.com>
2021-11-15 23:52:17 +0100

    messeging works

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 const socket = io("/")
 const sendBtn = document.querySelector(".send-btn");
+const messageBox = document.querySelector(".message-box");
+const messageContainer = document.querySelector(".message-container");
 const peer = new Peer(undefined, {
     host: '/',
     port: '3001'
@@ -19,20 +21,60 @@ socket.on("user-connected", userId => {
     console.log(userId + " Joined")
     connections.push(userId)
     conn = peer.connect(connections[0])
-    setTimeout(()=>{conn.send(userId + ":" + "^^^°°°~~~")},200)
+    setTimeout(()=>{conn.send(userId + ":" + "Connection Succesful")},200)
 })
 
 
 sendBtn.addEventListener("click", ()=>{
-    conn.send(userId + ":" + "hi")
+    conn.send(userId + ":" + messageBox.value)
+    messageBox.value = "";
 })
 
 peer.on('connection', function(connection) {
     connection.on('data', function(data){
         newUserId = data.substring(0, data.indexOf(":"))
         conn = peer.connect(newUserId)
-        console.log(data.replace(":","").replace(newUserId,""));
+        createPeerMsg(data.replace(":","").replace(newUserId,""),newUserId);
     });
   });
 
 
+function createUsrMsg(msg, usrId){
+    var msgContainer = document.createElement("div");
+    var usrMsg = document.createElement("div");
+    var msgTxt = document.createElement("p");
+    var usrName = document.createElement("a");
+
+    msgContainer.className += ("msg-container");
+    usrMsg.className += ("usr-msg");
+
+    msgTxt.innerHTML = msg;
+    usrName.innerHTML = usrId;
+
+    usrMsg.appendChild(msgTxt);
+    usrMsg.appendChild(usrName);
+    msgContainer.appendChild(usrMsg);
+    messageContainer.appendChild(msgContainer);
+    msgContainer.scrollIntoView();
+
+}
+
+function createPeerMsg(msg, usrId){
+    var msgContainer = document.createElement("div");
+    var usrMsg = document.createElement("div");
+    var msgTxt = document.createElement("p");
+    var usrName = document.createElement("a");
+
+    msgContainer.className += ("msg-container");
+    usrMsg.className += ("peer-msg");
+
+    msgTxt.innerHTML = msg;
+    usrName.innerHTML = usrId;
+
+    usrMsg.appendChild(msgTxt);
+    usrMsg.appendChild(usrName);
+    msgContainer.appendChild(usrMsg);
+    messageContainer.appendChild(msgContainer);
+    msgContainer.scrollIntoView();
+
+}
