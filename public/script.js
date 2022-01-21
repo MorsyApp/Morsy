49bc570fdc677c60ea22a4863f420a343cda86bc
Carl Marino <carlcmarino@protonmail.com>
2022-01-22 00:22:50 +0100

    cleaned up some code

--- public/script.js
+++ public/script.js
@@ -1,18 +1,6 @@
 ///vars
 const socket = io("/")
-const sendBtn = document.querySelector(".send-btn");
-const messageBox = document.querySelector(".message-box");
-const messageContainer = document.querySelector(".message-container");
-const fileBtn = document.querySelector(".file-btn");
-const fileInp = document.querySelector(".file-input");
-const usernameInp = document.querySelector(".username-inp");
-const editNameBtn = document.querySelector(".edit-name-btn");
-const inviteLinkBox = document.querySelector(".invite-link-box");
-const copyInviteBtn = document.querySelector(".copy-invite-btn");
-const inviteCloseBtn = document.querySelector(".invite-close-btn");
-const invitePopup = document.querySelector(".invite-popup");
-const inviteBtn = document.querySelector(".invite-btn");
-const typingMsg = document.querySelector(".is-typing-msg");
+
 let peerHostname = '127.0.0.1';
 let peerPort = 3001
 
@@ -26,7 +14,7 @@ const peer = new Peer(undefined, {
     port: peerPort,
 })
 
-inviteLinkBox.value= window.location.href;
+
 
 
 
@@ -61,9 +49,7 @@ socket.on("user-connected", userId => {
 })
 
 
-sendBtn.addEventListener("click", ()=>{
-    Send();
-})
+
 
 
 //peerjs managing connnections and data sent from other users
@@ -74,62 +60,3 @@ peer.on('connection', function(connection) {
   });
 
 
-
-//detects when enter is pressed while input is selected
-messageBox.addEventListener("keydown", (e)=>{
-    messageBoxEventHandler(e);
-
-})
-
-//detects when the file button is clicked
-fileBtn.addEventListener("click", ()=>{
-    fileInp.click();
-})
-
-//detects when the user selected a file and sends it
-fileInp.addEventListener("change", ()=>{
-    const reader = new FileReader();
-    reader.readAsDataURL(fileInp.files[0]);
-    reader.addEventListener("load", ()=>{
-
-        SendFile(reader.result)
-        fileInp.value = "";
-        
-    })
-
-
-})
-
-editNameBtn.addEventListener("click", () =>{
-    if(!editingName){
-        editNameBtn.innerHTML = "Save";
-        usernameInp.disabled = false;
-        usernameInp.focus();
-        editingName = true;
-    }
-    else{
-        editNameBtn.innerHTML = "Edit";
-        usernameInp.disabled = true;
-        SendUsrName(removeTags(usernameInp.value));
-        editingName = false;
-    }
-})
-
-copyInviteBtn.addEventListener("click", ()=>{
-    navigator.clipboard.writeText(inviteLinkBox.value);
-})
-
-inviteCloseBtn.addEventListener("click",()=>{
-    invitePopup.style.display = "none";
-})
-
-inviteBtn.addEventListener("click",()=>{
-    invitePopup.style.display = "block";
-    console.log("block")
-})
-
-window.addEventListener("mousedown", (e)=>{
-    if(!invitePopup.contains(e.target) && invitePopup.style.display == "block"){
-        invitePopup.style.display = "none";
-    }
-})
